ff86085d5e9a0787bb635806ab06687c3b2afe09
Michael Brown <160866598+Redcloudish@users.noreply.github.com>
2024-07-10 20:14:15 -0400

    polish

---
+++ MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by mike brown on 2024-07-10.
+//
+//
+
+import Foundation
+import CoreData
+
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
